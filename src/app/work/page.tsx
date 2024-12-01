845905220e0391554e3769d38b2ea300c9693863
code0wizard <140090259+code0wizard@users.noreply.github.com>
2024-12-01 16:26:17 +0100

    done

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { motion } from "framer-motion";
 import React, { useState } from "react";
-import { Swiper as SwiperType, SwiperSlide } from "swiper/react";
+import { Swiper , SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { BsArrowUpRight, BsGithub } from "react-icons/bs";
 import {
